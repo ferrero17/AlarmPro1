ca04743363d0e475979730ae3c04ff6dd11ff487
dferrero92 <dferrerol92@elpuig.xeill.net>
2018-11-18 20:51:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dani/alarmpro1/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.dani.alarmpro1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.ajustesBoton); // Creo una varibale boton i la asigno el boton de ajustes.
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AjustesActivity.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+        Button btnAjustesAlarma = (Button) findViewById(R.id.configuracionAlarma);
+        btnAjustesAlarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),ConfiguracionAlarmaActivity.class);
+                startActivityForResult(intent,0);
+            }
+        });
+
+
+
+
+    }
+}
