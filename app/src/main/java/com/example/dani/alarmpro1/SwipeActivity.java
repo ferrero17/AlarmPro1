2ddeb4e59394186a971b12cdb21b64210daeb8ec
dferrero92 <dferrerol92@elpuig.xeill.net>
2018-12-04 18:41:21 +0100

    aavfghnhfgnfghdfbfdbfdefewevdvd

--- app/src/main/java/com/example/dani/alarmpro1/SwipeActivity.java
+++ app/src/main/java/com/example/dani/alarmpro1/SwipeActivity.java
@@ -133,6 +133,11 @@ public class SwipeActivity extends AppCompatActivity {
              return new Fragment();
         }
 
+
+
+
+
+
         @Override
         public int getCount() {
             // Show 3 total pages.
