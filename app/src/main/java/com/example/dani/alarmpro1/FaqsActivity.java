7d2f534bbcefed7216e97436e76063ae6edbb1a9
dferrero92 <dferrerol92@elpuig.xeill.net>
2018-12-11 15:25:14 +0100

    Commit

---
+++ app/src/main/java/com/example/dani/alarmpro1/FaqsActivity.java
@@ -0,0 +1,26 @@
+package com.example.dani.alarmpro1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FaqsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faqs);
+
+       /* Button btnCambioTemaAzul = (Button) findViewById(R.id.botonTemaAzul);
+        btnCambioTemaAzul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/dani/alarmpro1/RepeatFragment.java
@@ -0,0 +1,24 @@
+package com.example.dani.alarmpro1;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RepeatFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_repeat, container, false);
+    }
+
+
+}
